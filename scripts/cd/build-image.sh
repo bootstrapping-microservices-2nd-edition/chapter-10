78b3c0922a6663b907ac69b0b89fb5c0463fca70
Ashley Davis <ashley@codecapers.com.au>
2023-03-23 10:37:11 +1000

    Added configuration for the GitHub Actions CD pipeline.

---
+++ scripts/cd/build-image.sh
@@ -0,0 +1,22 @@
+#
+# Builds a Docker image.
+#
+# Environment variables:
+#
+#   CONTAINER_REGISTRY - The hostname of your container registry.
+#   VERSION - The version number to tag the images with.
+#   NAME - The name of the image to build.
+#   DIRECTORY - The directory form which to build the image.
+#
+# Usage:
+#
+#       ./scripts/cd/build-image.sh
+#
+
+set -u # or set -o nounset
+: "$CONTAINER_REGISTRY"
+: "$VERSION"
+: "$NAME"
+: "$DIRECTORY"
+
+docker build -t $CONTAINER_REGISTRY/$NAME:$VERSION --file ../../$DIRECTOR/Dockerfile-prod ../../$DIRECTORY
