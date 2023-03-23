4cdcee4cc2b0f420c1606322bac7a53dd8dfc0c3
Ashley Davis <ashley@codecapers.com.au>
2023-03-23 10:43:24 +1000

    Fixed a broken shell script.

--- scripts/cd/build-image.sh
+++ scripts/cd/build-image.sh
@@ -19,4 +19,4 @@ set -u # or set -o nounset
 : "$NAME"
 : "$DIRECTORY"
 
-docker build -t $CONTAINER_REGISTRY/$NAME:$VERSION --file ../../$DIRECTOR/Dockerfile-prod ../../$DIRECTORY
+docker build -t $CONTAINER_REGISTRY/$NAME:$VERSION --file ./$DIRECTORY/Dockerfile-prod ./$DIRECTORY
