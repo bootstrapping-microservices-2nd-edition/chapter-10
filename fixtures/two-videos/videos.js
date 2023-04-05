91c20fea224d83cc07d1633cbf2ccd59fff320ca
Ashley Davis <ashley@codecapers.com.au>
2023-04-06 07:27:32 +1000

    Revised code so it doesn't go off page in the code listing.

--- fixtures/two-videos/videos.js
+++ fixtures/two-videos/videos.js
@@ -1,12 +1,12 @@
-const mongodb = require("mongodb");
+const { ObjectId } = require("mongodb");
 
 module.exports = [
     {
-        _id: mongodb.ObjectId("5ea234a1c34230004592eb32"),
+        _id: ObjectId("5ea234a1c34230004592eb32"),
         name: "SampleVideo_1280x720_1mb.mp4"
     },
     {
-        _id: mongodb.ObjectId("5ea234a5c34230004592eb33"),
+        _id: ObjectId("5ea234a5c34230004592eb33"),
         name: "Another video.mp4"
     }
 ];
