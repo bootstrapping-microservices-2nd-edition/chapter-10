c3fa7e29a90eccdd804093a63555fb4277d67982
Ashley Davis <ashley@codecapers.com.au>
2023-09-04 08:09:56 +1000

    Added "new" for creating ObjectIds, this seems to be required by the latest version of MongoDB.

--- fixtures/two-videos/videos.js
+++ fixtures/two-videos/videos.js
@@ -2,11 +2,11 @@ const { ObjectId } = require("mongodb");
 
 module.exports = [
     {
-        _id: ObjectId("5ea234a1c34230004592eb32"),
+        _id: new ObjectId("5ea234a1c34230004592eb32"),
         name: "SampleVideo_1280x720_1mb.mp4"
     },
     {
-        _id: ObjectId("5ea234a5c34230004592eb33"),
+        _id: new ObjectId("5ea234a5c34230004592eb33"),
         name: "Another video.mp4"
     }
 ];
